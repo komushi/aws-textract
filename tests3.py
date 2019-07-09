ec205ca5c8ceb87e6486034f3ee52b815909bf35
komushi <komushi@gmail.com>
2019-07-09 12:09:37 +0900

    init

---
+++ tests3.py
@@ -0,0 +1,102 @@
+import boto3
+import sys
+import re
+import json
+
+# get the results
+client = boto3.client(
+         service_name='textract',
+         region_name= 'us-west-2',
+         endpoint_url='https://textract.us-west-2.amazonaws.com',
+)
+
+
+def get_kv_map():
+
+    # process using image bytes
+    response = client.analyze_document(
+        S3Object={
+            'Bucket': 'storage-gocheckin-dev',
+            'Name': 'private/ap-northeast-1:e2205b24-6af1-4ca6-9177-d23a5ba2561b/xulei/b8MB10/XsZLmR/id/1.jpg'
+        }, 
+        FeatureTypes=['FORMS']
+    )
+
+    # Get the text blocks
+    blocks=response['Blocks']
+    
+
+    # get key and value maps
+    key_map = {}
+    value_map = {}
+    block_map = {}
+    for block in blocks:
+        block_id = block['Id']
+        block_map[block_id] = block
+        if block['BlockType'] == "KEY_VALUE_SET":
+            if 'KEY' in block['EntityTypes']:
+                key_map[block_id] = block
+            else:
+                value_map[block_id] = block
+
+    return key_map, value_map, block_map
+
+
+def get_kv_relationship(key_map, value_map, block_map):
+    kvs = {}
+    for block_id, key_block in key_map.items():
+        value_block = find_value_block(key_block, value_map)
+        key = get_text(key_block, block_map)
+        val = get_text(value_block, block_map)
+        kvs[key] = val
+    return kvs
+
+
+def find_value_block(key_block, value_map):
+    for relationship in key_block['Relationships']:
+        if relationship['Type'] == 'VALUE':
+            for value_id in relationship['Ids']:
+                value_block = value_map[value_id]
+    return value_block
+
+
+def get_text(result, blocks_map):
+    text = ''
+    if 'Relationships' in result:
+        for relationship in result['Relationships']:
+            if relationship['Type'] == 'CHILD':
+                for child_id in relationship['Ids']:
+                    word = blocks_map[child_id]
+                    if word['BlockType'] == 'WORD':
+                        text += word['Text'] + ' '
+                    if word['BlockType'] == 'SELECTION_ELEMENT':
+                        if word['SelectionStatus'] == 'SELECTED':
+                            text += 'X '    
+
+                                
+    return text
+
+
+def print_kvs(kvs):
+    for key, value in kvs.items():
+        print(key, ":", value)
+
+
+def search_value(kvs, search_key):
+    for key, value in kvs.items():
+        if re.search(search_key, key, re.IGNORECASE):
+            return value
+
+
+# MAIN PROGRAM
+key_map, value_map, block_map = get_kv_map()
+
+# Get Key Value relationship
+kvs = get_kv_relationship(key_map, value_map, block_map)
+print("\n\n== FOUND KEY : VALUE pairs ===\n")
+print_kvs(kvs)
+
+# Start searching a key value
+# while input('\n Do you want to search a value for a key? (enter "n" for exit) ') != 'n':
+#     search_key = input('\n Enter a search key:')
+#     print('The value is:', search_value(kvs, search_key))
